87ba414dc4a6080997462452a7126e1a8871a8d7
Carlo Zenesini <carlo.zenesini@mail.polimi.it>
2023-04-27 18:53:59 +0200

    Started adding the RMI communication's classes

--- src/main/java/org/gamein/model/Tile.java
+++ src/main/java/org/gamein/model/Tile.java
@@ -1,8 +1,9 @@
 package org.gamein.model;
 
+import java.io.Serializable;
 import java.util.Optional;
 
-public class Tile {
+public class Tile implements Serializable {
     private Integer tileID;
     private TileType tileType;
     
