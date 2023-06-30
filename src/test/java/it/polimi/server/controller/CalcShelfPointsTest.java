9f16ed7e09075116a31cb3bad811846eb32d3fe0
no-dady <daniel.shala@mail.polimi.it>
2023-06-30 02:35:15 +0200

    Test adjacent tiles point calculation

---
+++ src/test/java/it/polimi/server/controller/CalcShelfPointsTest.java
@@ -0,0 +1,96 @@
+package it.polimi.server.controller;
+
+import com.google.gson.reflect.TypeToken;
+import it.polimi.server.cgc.StraightDirection;
+import it.polimi.server.cgc.StraightDirectionCGCTest;
+import it.polimi.server.model.Bookshelf;
+import it.polimi.server.model.Tile;
+import it.polimi.util.Parser;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalcShelfPointsTest {
+
+
+    public static void printShelf(Tile[][] shelf) {
+        System.out.println();
+        for(int i = 5; i >= 0; i--) {
+            System.out.print("|\t");
+            for(int j = 0; j < 5; j++) {
+                System.out.print(shelf[i][j].getTileType() + "\t|\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static List<Tile[][]> getTilesForTest(String jsonPath) throws IOException {
+        BufferedReader reader = Files.newBufferedReader(Paths.get(Parser.getResourcePath(jsonPath)));
+        String json = reader.lines().collect(Collectors.joining());
+        return Parser.fromJson(json, new TypeToken<List<Tile[][]>>(){}.getType());
+    }
+    @Test
+    void calculateBookshelfPoints() throws IOException {
+        String path = "json/shelves/StraightDirectionTest";
+
+        List<Tile[][]> shelf = StraightDirectionCGCTest.getTilesForTest(path + "/testOne.json");
+        assertEquals(0, calculateBookshelfPoints(shelf.get(0)));
+        assertEquals(14, calculateBookshelfPoints(shelf.get(1)));
+        assertEquals(8, calculateBookshelfPoints(shelf.get(2)));
+    }
+
+    private boolean isValidTileBookshelf(int row, int col) {
+        return row >= 0 && row < Bookshelf.getRows() && col >= 0 && col < Bookshelf.getCols();
+    }
+    private int exploreAdjacentTiles(int row, int col, boolean[][] visited, Tile[][] slots) {
+        int consecutiveTiles = 1;
+        Tile currentTile = slots[row][col];
+        visited[row][col] = true;
+
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
+
+        for (int i = 0; i < 4; i++) {
+            int newRow = row + dx[i];
+            int newCol = col + dy[i];
+
+            if (isValidTileBookshelf(newRow, newCol) && !visited[newRow][newCol]) {
+                Tile adjacentTile = slots[newRow][newCol];
+                if (adjacentTile != null && adjacentTile.equals(currentTile)) {
+                    consecutiveTiles += exploreAdjacentTiles(newRow, newCol, visited, slots);
+                }
+            }
+        }
+        return consecutiveTiles;
+    }
+    private int calculateBookshelfPoints(Tile[][] bookshelf){
+        int points = 0;
+        Tile[][] slots = bookshelf;
+        boolean[][] visited = new boolean[Bookshelf.getRows()][Bookshelf.getCols()];
+        for (int i = 0; i < Bookshelf.getRows(); i++) {
+            for (int j = 0; j < Bookshelf.getCols(); j++) {
+                if (slots[i][j] != null && !visited[i][j]) {
+                    int consecutiveTiles = exploreAdjacentTiles(i, j, visited, slots);
+                    if (consecutiveTiles >= 6) {
+                        points += 8;
+                    } else if (consecutiveTiles == 5) {
+                        points += 5;
+                    } else if (consecutiveTiles == 4) {
+                        points += 3;
+                    } else if (consecutiveTiles == 3) {
+                        points += 2;
+                    }
+                }
+            }
+        }
+        return points;
+    }
+}
